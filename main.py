9f173e37b825fd0a313e18100e64592d4994d83d
Redwane <redwanehammas@gmail.com>
2022-07-25 00:18:43 +0200

    Optimize spaceship rendering

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ SCREEN = pg.display.set_mode((WIDTH, HEIGHT))
 
 FPS = 60
 
-def init_window():
+def init_window(rec_1, rec_2):
     pg.display.set_caption('Shooter Game')
     SCREEN.fill((255, 255, 255))
-    SCREEN.blit(get_ship(1).get('sprite'), get_ship(1).get('position'))
-    SCREEN.blit(get_ship(2).get('sprite'), get_ship(2).get('position'))
+    SCREEN.blit(get_ship(1), (rec_1.x, rec_1.y))
+    SCREEN.blit(get_ship(2), (rec_2.x, rec_2.y))
     pg.display.update()
 
 
@@ -27,24 +27,21 @@ def get_ship(ship: int):
     )
     image = pg.transform.scale(image, (S_WIDTH, S_HEIGHT))
     image = pg.transform.rotate(image, (90 if ship == 1 else 270))
-    return {
-        'sprite': image,
-        'position': (abs((ship-1)*WIDTH - .15*WIDTH), HEIGHT/2.5)
-    }
+    return image
 
 
 def main():
-
     clock = pg.time.Clock()
 
+    rec_1 = pg.Rect(.15*WIDTH, HEIGHT/2.2, S_WIDTH, S_HEIGHT)
+    rec_2 = pg.Rect(.85*WIDTH, HEIGHT/2.2, S_WIDTH, S_HEIGHT)
 
     while True:
         clock.tick(FPS)
         for event in pg.event.get():
             if event.type == pg.QUIT: return pg.quit()
         
-        init_window()
-        
+        init_window(rec_1, rec_2)        
 
 
 if __name__ == '__main__':
